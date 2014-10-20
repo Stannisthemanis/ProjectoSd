dda77eea972f16678112b8fca1dd6d5c61e952f5
Diogo <rxkax@hotmail.com>
2014-10-20 17:45:09 +0100

    revolving user problems try fix 1

--- src/RmiServerInterface.java
+++ src/RmiServerInterface.java
@@ -12,36 +12,36 @@ public interface RmiServerInterface extends Remote {
 
     public boolean checkLogin(String username, String password) throws RemoteException;
 
-    public String getUpcumingMeetings(User user) throws RemoteException;
+    public String getUpcumingMeetings(String user) throws RemoteException;
 
-    public String getPassedMeetings(User user) throws RemoteException;
+    public String getPassedMeetings(String user) throws RemoteException;
 
-    public String getMeetingInfo(int flag, int nMeeting, User user) throws RemoteException;
+    public String getMeetingInfo(int flag, int nMeeting, String user) throws RemoteException;
 
-    public String getAgendaItemFromMeeting(int flag, int nMeeting, User user) throws RemoteException;
+    public String getAgendaItemFromMeeting(int flag, int nMeeting, String user) throws RemoteException;
 
-    public String getMessagesByUser(User user) throws RemoteException;
+    public String getMessagesByUser(String user) throws RemoteException;
 
-    public int getNumberOfMessages(User user) throws RemoteException;
+    public int getNumberOfMessages(String user) throws RemoteException;
 
-    public String getResumeOfMessage(User user, int message) throws RemoteException;
+    public String getResumeOfMessage(String user, int message) throws RemoteException;
 
-    public boolean setReplyOfInvite(User user, int message, boolean decision) throws RemoteException;
+    public boolean setReplyOfInvite(String user, int message, boolean decision) throws RemoteException;
 
-    public boolean addAgendaItem(int nMeeting, String newAgendaItem, User user) throws RemoteException;
+    public boolean addAgendaItem(int nMeeting, String newAgendaItem, String user) throws RemoteException;
 
-    public boolean removeAgendaItem(int nMeeting, int nAgenda, User user) throws RemoteException;
+    public boolean removeAgendaItem(int nMeeting, int nAgenda, String user) throws RemoteException;
 
-    public boolean modifyTitleAgendaItem(int nMeeting, int nAgenda, String mAgenda, User user) throws RemoteException;
+    public boolean modifyTitleAgendaItem(int nMeeting, int nAgenda, String mAgenda, String user) throws RemoteException;
 
-    public boolean addKeyDecisionToAgendaItem(int nMeeting, int nAgenda, String keyDecision, User user) throws RemoteException;
+    public boolean addKeyDecisionToAgendaItem(int nMeeting, int nAgenda, String keyDecision, String user) throws RemoteException;
 
-    public boolean addActionItem(int nMeeting, String actionItem, User user) throws RemoteException;
+    public boolean addActionItem(int nMeeting, String actionItem, String user) throws RemoteException;
 
-    public String getActionItemFromUser(User user) throws RemoteException;
+    public String getActionItemFromUser(String user) throws RemoteException;
 
-    public int getSizeOfTodo(User user) throws RemoteException;
+    public int getSizeOfTodo(String user) throws RemoteException;
 
-    public boolean setActionAsCompleted(User user, int n);
+    public boolean setActionAsCompleted(String user, int n) throws RemoteException;
 
 }
